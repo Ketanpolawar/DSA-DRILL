6b995521985cb576a5d23e733664b02674aa848e
Ketan Polawar <ketanpolawar@gmail.com>
2024-03-29 21:53:58 +0530

    Count set bit

---
+++ Bit manupulation/CountSetbit.java
@@ -0,0 +1,13 @@
+public static void main(String args []){
+    int A=10;
+    int B;
+    B=public static int CountSet(A);
+    System.out.println(B);
+}
+
+public static int CouSet(num){
+    while(num!=0){
+    num&=num-1;
+    }
+    retun num
+}
