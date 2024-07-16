892fa78efa2cfdd117c5bdc56ba9e4634dac0699
Ketan Polawar <ketanpolawar@gmail.com>
2024-07-16 20:06:09 +0530

    comment added

--- Stacks/count negs.py
+++ Stacks/count negs.py
@@ -2,7 +2,7 @@
 #     def count_NGEs(self, N, arr, queries):
 #         st = []
 #         res = [-1] * N  # Initialize result array with -1s
-        
+     
 #         # Step 1: Find Next Greater Elements using a stack
 #         for i in range(N - 1, -1, -1):  # Reverse iteration from end to start
 #             while st and st[-1] <= arr[i]:
@@ -13,7 +13,7 @@
         
 #         # Step 2: Calculate counts of NGEs from each index to the end
 #         nge_count = [0] * N
-#         for i in range(N - 2, -1, -1):
+#         for i in range(N - 2, -1, -1):#last element will be zero anyways 
 #             if res[i] != -1:
 #                 nge_count[i] = nge_count[i + 1] + 1
 #             else:
