8d8a5fdbc48d277bc42151e2384f83bb504a2c8d
Ketan Polawar <ketanpolawar@gmail.com>
2024-07-17 22:47:59 +0530

    p

--- ETL/GFG/factorial.py
+++ ETL/GFG/factorial.py
@@ -1,6 +1,26 @@
-def fact(n):
-    ans=1
-    for i in range(1,n+1):
-        ans=ans*i
-    return ans
-print(fact(5))
+# def fact(n):
+#     ans=1
+#     for i in range(1,n+1):
+#         ans=ans*i
+#     return ans
+# print(fact(5))
+
+
+
+
+# # Python 3 program to find  
+# # factorial of given number 
+# def factorial(n): 
+     
+#     # Checking the number
+#     # is 1 or 0 then
+#     # return 1
+#     # other wise return
+#     # factorial
+#     if (n==1 or n==0):
+         
+#         return 1
+     
+#     else:
+         
+#         return (n * factorial(n - 1)) 
